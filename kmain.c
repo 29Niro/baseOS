def1415dedc420686734bd40d647825e50f2b66b
29Niro <59716887+29Niro@users.noreply.github.com>
2021-08-06 08:27:04 +0530

    Integrate outputs (#5)
    * outb function created
    
    * outb function declaration
    
    * frame buffer header file created
    
    * Calling C function
    
    * C function was created
    
    * new object file added in Makefile
    
    * change int value in C function
    
    * object file created
    
    * change integer value
    
    * Modified files added

--- kmain.c
+++ kmain.c
@@ -1,5 +1,13 @@
+#include "io.h"
+#include "fb.h"
+
     /* The C function */
     int sum_of_three(int arg1, int arg2, int arg3)
     {
         return arg1 + arg2 + arg3;
+    }
+
+    void run(){
+        char c[] = "Welcome";
+        write(c, 9);
     }
