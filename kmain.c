925d5038eac300d9c82315e842702dc4a55ecf21
29Niro <29niro@gmail.com>
2021-08-27 19:37:43 +0530

    Old files are updated

--- kmain.c
+++ kmain.c
@@ -3,7 +3,7 @@
 #include "serial_port.h"
 #include "gdt.h"
 #include "interrupt/interrupts.c"
-
+#include "multiboot.h"
 
 
     void init()
@@ -12,11 +12,29 @@
 	interrupts_install_idt();
     }
 
-    void run(){
+    void run(unsigned int ebx){
 	init();
 
-        char c[] = "Welcome";
-        write(c, 9);
+	multiboot_info_t *mbinfo = (multiboot_info_t *) ebx;
+        multiboot_module_t* modules = (multiboot_module_t*) mbinfo->mods_addr; 
+        unsigned int address_of_module = modules->mod_start;
 
-        serial_write(SERIAL_COM1_BASE, c, 7);
+	if(mbinfo->mods_count == 1){
+		 char c[] = "Welcome";
+       		 write(c, 9);
+
+        	serial_write(SERIAL_COM1_BASE, c, 7);
+
+		typedef void (*call_module_t)(void);
+        	/* ... */
+        	call_module_t start_program = (call_module_t) address_of_module;
+        	start_program();
+        	/* we'll never get here, unless the module code returns */
+    	}
+	else{
+        char errorMsg[] = "Error: Number of modules loaded is not equal to 1.\n";
+        unsigned int err_len = sizeof(errorMsg) / sizeof(errorMsg[0]);
+        write(errorMsg, err_len);
+        serial_write(SERIAL_COM1_BASE, errorMsg, err_len);
+    	}
     }
--- loader.s
+++ loader.s
@@ -1,8 +1,8 @@
     global loader                   ; the entry symbol for ELF
 
     MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
-    FLAGS        equ 0x0            ; multiboot flags
-    CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
+    FLAGS        equ 0x00000001     ; multiboot flags / align modules
+    CHECKSUM     equ -(MAGIC_NUMBER + FLAGS)  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
     KERNEL_STACK_SIZE equ 4096                  ; size of stack in bytes
 
@@ -21,6 +21,9 @@
         mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
         mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                     ; stack (end of memory area)
+        
+	push ebx
+
         ; Calling C code
         extern run
         call run        
